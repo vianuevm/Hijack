0e305d1da68b4df95d24993da159f8b828ce8fca
Mike Vianueva <vianuevm@umich.edu>
2015-02-18 20:54:17 -0500

    Initial plane with Isometric camera and tank.  Press space to fire balls of steel

---
+++ Hijack/Assets/_Scripts/TankMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class TankMovement : MonoBehaviour {
+	public float speed;
+	public Vector3 position;
+	public CharacterController charControl;
+	public GameObject cannonBall;
+	public GameObject shotLocation;
+	public List<GameObject>  cannonBallShots;
+	// Use this for initialization
+	void Start () {
+		speed = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow)){
+			charControl.SimpleMove(transform.forward * speed);
+		}  else if(Input.GetKey(KeyCode.DownArrow)){
+			charControl.SimpleMove(transform.forward * speed * -1f);
+		}	
+
+		if (Input.GetKey(KeyCode.DownArrow)) transform.Translate(0, 0, -1* Time.deltaTime);
+		if (Input.GetKey(KeyCode.UpArrow)) transform.Translate(0, 0, 1* Time.deltaTime);
+		if (Input.GetKey(KeyCode.RightArrow)) transform.Rotate(0, 1, 0);
+		if (Input.GetKey(KeyCode.LeftArrow)) transform.Rotate(0, -1, 0);
+
+	}
+	
+	void FixedUpdate(){
+		if(Input.GetKeyDown(KeyCode.Space)){
+			GameObject shot = Instantiate(cannonBall) as GameObject;
+			shot.transform.position = shotLocation.transform.position;
+			shot.rigidbody.AddForce(transform.forward * 500f);
+			cannonBallShots.Add(shot);
+			
+		}
+	}
+	
+}
