049cd799a70b3e63cc2028a886e0a7a95d7bb01a
Mike Vianueva <vianuevm@umich.edu>
2015-02-21 17:44:25 -0500

    Made base vehicle class

---
+++ Hijack/Assets/_Scripts/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vehicle : MonoBehaviour {
+	public float vehicleHealth;
+	public float vehicleSpeed;
+	public float vehicleArmor;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Attack(){
+
+	}
+
+	void Hurt(){
+
+	}
+}
