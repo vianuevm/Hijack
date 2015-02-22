9ab9391c871bcca4d612f447bdd0e2d98cbc7ab2
yunhanwei <yunhanw@umich.edu>
2015-02-21 21:54:44 -0500

    actual humvee push...

--- Hijack/Assets/_Scripts/Humv.cs
+++ Hijack/Assets/_Scripts/Humv.cs
@@ -3,34 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 public class Humv : Vehicle {
 	public float speed;
+	public float rotate_speed;
 	public Vector3 position;
 	//public CharacterController charControl;
-	public GameObject cannonBall;
+	public GameObject bullet;
 	public GameObject shotLocation;
-	public List<GameObject>  cannonBallShots;
+	//public List<GameObject>  cannonBallShots;
 	// Use this for initialization
 	void Start () {
 		speed = 10f;
+		rotate_speed = 3f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		if (Input.GetKey(KeyCode.DownArrow)) transform.Translate(0, 0, -speed* Time.deltaTime);
-		if (Input.GetKey(KeyCode.UpArrow)) transform.Translate(0, 0, speed* Time.deltaTime);
-		if (Input.GetKey(KeyCode.RightArrow)) transform.Rotate(0, 1, 0);
-		if (Input.GetKey(KeyCode.LeftArrow)) transform.Rotate(0, -1, 0);
-		
-	}
-	
-	void FixedUpdate(){
+		if(Input.GetKey(KeyCode.UpArrow)){
+			transform.rigidbody.velocity = (transform.forward * speed);
+		}  else if(Input.GetKey(KeyCode.DownArrow)){
+			transform.rigidbody.velocity = -1 * (transform.forward * speed);
+		}	
+		if (Input.GetKey(KeyCode.RightArrow)) transform.Rotate(0, rotate_speed, 0);
+		if (Input.GetKey(KeyCode.LeftArrow)) transform.Rotate(0, -rotate_speed, 0);
+
+
 		if(Input.GetKeyDown(KeyCode.Space)){
-			GameObject shot = Instantiate(cannonBall) as GameObject;
+			GameObject shot = Instantiate(bullet) as GameObject;
 			shot.transform.position = shotLocation.transform.position;
-			shot.rigidbody.AddForce(transform.forward * 500f);
-			cannonBallShots.Add(shot);
+			shot.rigidbody.velocity = transform.forward * 18f;
 			
 		}
 	}
-	
 }
