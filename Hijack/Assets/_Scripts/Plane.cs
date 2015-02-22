2fcce409e9794aae10e934a3ccfd1bbdd35663b1
Mike Vianueva <vianuevm@umich.edu>
2015-02-21 20:56:50 -0500

    more shit

--- Hijack/Assets/_Scripts/Plane.cs
+++ Hijack/Assets/_Scripts/Plane.cs
@@ -1,15 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class Plane : MonoBehaviour {
-
+using System.Collections.Generic;
+public class Plane : Vehicle {
+	public float speed;
+	public Vector3 position;
+	public CharacterController charControl;
+	public GameObject cannonBall;
+	public GameObject shotLocation;
+	public List<GameObject>  cannonBallShots;
 	// Use this for initialization
 	void Start () {
-	
+		speed = 4f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKey(KeyCode.UpArrow)){
+			transform.rigidbody.velocity = (transform.forward * speed);
+		}  else if(Input.GetKey(KeyCode.DownArrow)){
+			transform.rigidbody.velocity = -1 * (transform.forward * speed);
+		} else {
+			transform.rigidbody.velocity = Vector3.zero;
+		}
+		
+		if (Input.GetKey(KeyCode.RightArrow)) transform.Rotate(0, 1, 0);
+		if (Input.GetKey(KeyCode.LeftArrow)) transform.Rotate(0, -1, 0);
+		
 	}
+	
+	void FixedUpdate(){
+		if(Input.GetKeyDown(KeyCode.Space)){
+			GameObject shot = Instantiate(cannonBall) as GameObject;
+			shot.transform.position = shotLocation.transform.position;
+			shot.rigidbody.AddForce(transform.forward * 2000f);
+			cannonBallShots.Add(shot);
+		}
+		for(int i = 0; i < cannonBallShots.Count; ++i){
+			if(Mathf.Abs(cannonBallShots[i].transform.position.x - transform.position.x) > 10f && !cannonBallShots[i].renderer.isVisible){
+				Destroy(cannonBallShots[i]);
+				cannonBallShots.RemoveAt(i);
+				i-= 1;
+			}
+		}	
+	}
+	
 }
